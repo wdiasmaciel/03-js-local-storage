8f3e5fdf457a62cdec6d6d1bac1a2131a50e1e56
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-07 19:45:11 +0000

    Exemplo

--- funcionalidade.js
+++ funcionalidade.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Salvar a lista atualizada no LocalStorage:
         localStorage.setItem('usuarios', JSON.stringify(usuarios));
         exibirUsuarios();
-        entradaNome = entradaIdade = entradaCurso = "";
+        entradaNome.value = entradaIdade.value = entradaCurso.value = "";
     }
 
     // Função para exibir a lista de usuários salvos:
